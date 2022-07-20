5b6f7795c77a1959b3ae3bb6fcc14d737afabc32
Lucas Kaique <lucaskaique743@gmail.com>
2022-07-20 18:20:30 -0300

    Adcionando as boas vindas

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,10 @@ setInterval(
     function relogio() {
 
     var agora = new Date();
-    
+
+
+    var boasVindas = document.getElementById('texto');
+
     var hora = agora.getHours();
     var minuto = agora.getMinutes();
     var segundos = agora.getSeconds();
@@ -14,6 +17,16 @@ setInterval(
     hor.innerHTML = hora + ' h'
     min.innerHTML = minuto + ' m'
     sec.innerHTML = segundos + ' S'
+
+    if(hora >= 5 && hora < 12){
+        boasVindas.innerHTML = 'Bom Dia'
+    } else if (hora >= 12 && hora <= 18){
+        boasVindas.innerHTML = 'Boa tarde'
+    } else if (hora > 18 && hora <= 23 || hora === 00){
+        boasVindas.innerHTML = 'Boa noite'
+    } else {
+        boasVindas.innerHTML = 'Boa madrugada'
+    }
 }
 )
 
