e8150b696f73a136496a76034315332626e79a93
Lucas Kaique <lucaskaique743@gmail.com>
2022-07-21 20:07:22 -0300

    Adcionando data

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,50 @@ setInterval(
 
     var agora = new Date();
 
+    var mostrarData = document.getElementById('mostra');
+
+    var mes = agora.getUTCMonth() + 1;
+
+    switch(mes) {
+        case 1: 
+           var mesExtenso = ('Janeiro');
+            break;
+        case 2:
+           var mesExtenso = ('Fevereiro');
+            break;
+        case 3: 
+           var mesExtenso = ('Março');
+            break;
+        case 4: 
+           var mesExtenso = ('Abril');
+            break;
+        case 5: 
+           var mesExtenso = ('Maio');
+            break;
+        case 6: 
+           var mesExtenso = ('Junho');
+            break;
+        case 7: 
+           var mesExtenso = ('Julho');
+            break;
+        case 8: 
+           var mesExtenso = ('Agosto');
+            break;
+        case 9: 
+           var mesExtenso = ('Setembro');
+            break;
+        case 10: 
+           var mesExtenso = ('Outubro');
+            break;
+        case 11: 
+           var mesExtenso = ('Novembro');
+            break;
+        case 12: 
+           var mesExtenso = ('Dezembro');
+            break;
+    }
+
+    mostrarData.innerHTML = `${agora.getDate()} de ` + `${mesExtenso}/` + `${agora.getFullYear()}`;
 
     var boasVindas = document.getElementById('texto');
 
@@ -19,7 +63,7 @@ setInterval(
     sec.innerHTML = segundos + ' S'
 
     if(hora >= 5 && hora < 12){
-        boasVindas.innerHTML = 'Bom Dia'
+        boasVindas.innerHTML = 'Bom dia'
     } else if (hora >= 12 && hora <= 18){
         boasVindas.innerHTML = 'Boa tarde'
     } else if (hora > 18 && hora <= 23 || hora === 00){
